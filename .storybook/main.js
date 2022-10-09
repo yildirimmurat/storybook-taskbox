74be30251bd974f9f04275c0566e1c7d03eae230
Murat Yildirim <murat@lemonbrain.ch>
2022-10-09 04:38:07 +0200

    added simple component

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,10 +1,11 @@
 module.exports = {
-  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  stories: ["../src/components/**/*.stories.js"],
   staticDirs: ["../public"],
   addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
+    '@storybook/addon-a11y',
   ],
   framework: "@storybook/vue3",
   core: {
